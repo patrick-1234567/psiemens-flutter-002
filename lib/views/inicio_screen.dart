c7f0d29d001a931c01fb3d326b7e738a2441f466
patrick-1234567 <patricksiemens12@gmail.com>
2025-04-03 18:45:26 -0300

    Prompt 3
    añade botones en incio para navegar entre
    pantallas

--- lib/views/inicio_screen.dart
+++ lib/views/inicio_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'acercade_screen.dart';
+import 'contacto_screen.dart';
 
 class InicioScreen extends StatelessWidget {
   @override
@@ -6,9 +8,33 @@ class InicioScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Inicio')),
       body: Center(
-        child: Text(
-          'Bienvenido',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Bienvenido',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AcercaDeScreen()),
+                );
+              },
+              child: Text('Ir a Acerca de'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ContactoScreen()),
+                );
+              },
+              child: Text('Ir a Contacto'),
+            ),
+          ],
         ),
       ),
     );
